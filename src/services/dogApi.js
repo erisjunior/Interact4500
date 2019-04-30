338286b530ff2bda6d20c153c9fc8842825379ca
Erisvan Junior <erisvan.junior.a@gmail.com>
2019-04-29 23:43:36 -0300

    settuping

---
+++ src/services/dogApi.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const dogApi = axios.create({
+  baseURL: "https://api.thedogapi.com/v1",
+  headers: { "x-api-key": "e950ce28-f1da-4f38-b95f-52e2ec492dd7" }
+});
+
+export default api;
